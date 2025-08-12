2882b460385f5cbdcf193a66c523327f063b53b5
ka-few <njorovista@gmail.com>
2025-08-12 15:58:00 +0300

    Inheritance lab pt 1

--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,16 @@ from user import User
 import random
 
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = [
+            "Python is a versatile programming language.",
+            "Object-oriented programming is a paradigm.",
+            "Inheritance allows code reuse.",
+            "Polymorphism enables flexible code.",
+            "Encapsulation protects data."
+        ]
 
     def teach(self):
-        pass
+        import random
+        return random.choice(self.knowledge)
